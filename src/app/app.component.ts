0ec943b8fbbce73480c55e699f45955b7f8aff9a
Fabiana1vo <fabianasouzaivo@icloud.com>
2023-02-28 17:55:47 -0300

    branch certa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import KeenSlider, { KeenSliderInstance } from 'keen-slider';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss',
+    '../../node_modules/keen-slider/keen-slider.min.css'
+  ]
 })
 export class AppComponent {
   title = 'Nu';
+
+  @ViewChild('sliderRef')
+  sliderRef!:ElementRef<HTMLElement>
+  @ViewChild('cardsliderRef')
+  cardsliderRef!:ElementRef<HTMLElement>
+  
+ 
+
+  slider?: KeenSliderInstance
+  cardSliderRef?: KeenSliderInstance
+
+  ngAfterViewInit() {
+this.slider = new KeenSlider(this.sliderRef.nativeElement,{
+  loop:false,
+  mode:'free',
+  slides:{
+    perView: 4.5,
+    spacing:14
+  }
+});
+this.cardsliderRef = new KeenSlider(this.cardsliderRef.nativeElement,{
+  loop:false,
+  mode:'free',
+  slides:{
+    perView: 1.5,
+    spacing:14
+  }
+})
+
+  }
 }
